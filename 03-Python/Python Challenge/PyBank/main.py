e12938a5ec8e051dac33bd6233f4adf88c02282e
Tigist Keneni <Tig@Tigists-MacBook-Air.local>
2020-09-16 15:44:08 -0700

    solved 04-Pandas

--- 03-Python/Python Challenge/PyBank/main.py
+++ 03-Python/Python Challenge/PyBank/main.py
@@ -5,26 +5,32 @@ from pathlib import Path
 #employee_data = os.path.join(os.getcwd(), 'Resources/budget_data.csv')
 budget_csv = os.path.join(Path(__file__).parent,"Resources","budget_data.csv")
 
+print ("Financial Analysis")
+print ("----------------------------")
+
 #toal amount of months
-file = open("budget_data.csv")
-reader = csv.reader(budget_data.csv)
-lines= len(list(reader))
+total_months = sum(1 for row in budget_csv)
+print ("Total Months:" + str(total_months -1)) #-1 to remove the header
 
-total_monhs = csv.reader len(list(reader))
-print(total_months)
+#total amount of the changes in "Profit/Losses" over the entire period
+total = df['Losses'].sum(budget_csv) 
+print ("Total:" + str(total)
 
-with open (budget_csv,'r') as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter = ",")
-    for row in csv_reader:
-        print(', '.join(row))
+#total = sum (1 for row in ["Losses" in budget_csv])
+#print ("Total:" + str(total)
 
 
+#for row in budget_csv:
+   # _dist = row[2]
+ #   try: 
+  #      _dist = float(_dist)
+  #  except ValueError:
+  #      _dist = 0
 
+  #  dist += _dist
 
-
-
-
-
-
-
-
+#csvpath = os.path.join("budget_data.csv")
+#with open (budget_csv,'r') as csvfile:
+    #csv_reader = csv.file(csv_reader, delimiter = ',')
+    #csv_reader = next(csv_reader)
+    #print(', '.join(row)
